9a30ecfef8a6ccd8911717462cb35c9c8cc4539d
msunkist99 <msunkist99@gmail.com>
2020-10-18 10:19:19 -0500

    part 3 excercise complete without bonus

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { dir } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,74 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  saveColor = null;
+
+  takeOffEnabled: boolean = true;
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+       this.takeOffEnabled = false;
+    }
+ }
+
+  handleLand() {
+    window.alert('The shuttle is landing.  Landing gear engaged.');
+  
+    this.color = 'green';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'The shuttle has landed.';
+    this.takeOffEnabled = true;
+  }
+
+  handleAbort() {
+    let result = window.confirm('Are you sure you want to abort the shuttle mission?');
+    if (result) {
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Mission aborted.';
+      this.takeOffEnabled = true;
+    }
+  }
+
+  moveRocket(rocketImage, direction){
+    if (direction === 'right'){
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+
+    if (direction === 'left'){
+      rocketImage.style.left = parseInt(rocketImage.style.left) - 10 + 'px';
+      this.width = this.width - 10000;
+    }
+
+    if (direction === 'up') {
+      rocketImage.style.bottom = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      this.height = this.height + 10000;
+    }
+
+    if (direction === 'down'){
+      rocketImage.style.bottom = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      this.height = this.height - 10000;
+    }
+
+    console.log("rocketImage.style.left " + rocketImage.style.left + " rocketImage.style.bottom " + rocketImage.style.bottom);
+    this.checkEdges(rocketImage);
+  }
+
+  checkEdges(rocketImage){
+    if (parseInt(rocketImage.style.left) > 75 || parseInt(rocketImage.style.left) < 0 || parseInt(rocketImage.style.bottom) > 320 || parseInt(rocketImage.style.bottom) < 0) {
+      this.color = 'orange';
+    }
+    else {
+      this.color = 'blue';
+    }
+  }
 }
